1a0aead30cc7244c89027adc89f225dae85bd27d
mollyjeanbennett@gmail.com <mollyjeanbennett@gmail.com>
2018-06-19 20:15:53 +0000

    Modified 1 node:
    Updated function node tweeter

--- Stackery/functions/tweeter/index.js
+++ Stackery/functions/tweeter/index.js
@@ -3,10 +3,10 @@ const Twit = require('twit')
 module.exports = async message => {
 
   const T = new Twit({
-  consumer_key:         process.env.consumer_key,
-  consumer_secret:      process.env.consumer_secret,
-  access_token:         process.env.access_token,
-  access_token_secret:  process.env.access_token_secret
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 });
 
 tweetPoem()
